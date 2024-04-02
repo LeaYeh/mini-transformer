1b8cd23d28034fe1cb9df168fd74a3f96d8fc9ad
LeaYeh <smile2140@gmail.com>
2024-04-02 14:33:14 +0200

    feat: Mount current folder on jupyter service

--- run_dev.sh
+++ run_dev.sh
@@ -1,2 +1,2 @@
 docker build -t pytorch-jupyter-notebook .
-docker run -it --rm -p 8888:8888 pytorch-jupyter-notebook start-notebook.sh --NotebookApp.token='' --NotebookApp.password=''
+docker run -it --rm -p 8888:8888 -v .:/home/jovyan/work pytorch-jupyter-notebook start-notebook.sh --NotebookApp.token='' --NotebookApp.password=''
